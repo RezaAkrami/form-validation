c7646bbe6f6c00e52994cb99bd8b6350e15e3dca
Reza Akrami <RezaAkrami32@gmail.com>
2022-08-21 19:06:19 +0430

    Add html tags to the project

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,7 +3,37 @@ import React from 'react';
 const SignUp = () => {
     return (
         <div>
-            
+            <form>
+                <div>
+                    <label>Name</label>
+                    <input type="text" />
+                </div>
+                
+                <div>
+                    <label>Email</label>
+                    <input type="text" />
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input type="password" />
+                </div>
+
+                <div>
+                    <label>Confirm Password</label>
+                    <input type="password" />
+                </div>
+
+                <div>
+                    <label>Accept our policies</label>
+                    <input type="text" />
+                </div>
+
+                <div>
+                    <button type='submit'>Log in</button>
+                    <button type='submit'>Sign Up</button>
+                </div>
+            </form>
         </div>
     );
 };
