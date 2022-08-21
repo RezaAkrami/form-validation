09367dd5affb4adc738010b7749a63dd8ef21f21
Reza Akrami <RezaAkrami32@gmail.com>
2022-08-21 19:24:57 +0430

    set state and show values in input

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,14 +4,23 @@ const SignUp = () => {
 
     // state
     const [data, setData] = useState({
-
         name : "" ,
         email : "" ,
         password : "" ,
         confirmPassword : "" ,
         isAccepted : false ,
-
     });
+
+    //set state
+    function changeHandler(event) {
+        let target = event.target;
+
+        if( target.name === "isAccepted" ){
+            setData({ ...data , [target.name] : target.checked });
+        }else{
+            setData({ ...data , [target.name] : target.value });
+        }
+    }
 
     //destructuring data
     const {name,email,password,confirmPassword,isAccepted} = data;
@@ -21,27 +30,27 @@ const SignUp = () => {
             <form>
                 <div>
                     <label>Name</label>
-                    <input type="text" />
+                    <input type="text" name="name" value={name} onChange={changeHandler} />
                 </div>
                 
                 <div>
                     <label>Email</label>
-                    <input type="text" />
+                    <input type="text" name="email" value={email} onChange={changeHandler}/>
                 </div>
 
                 <div>
                     <label>Password</label>
-                    <input type="password" />
+                    <input type="password" name="password" value={password} onChange={changeHandler}/>
                 </div>
 
                 <div>
                     <label>Confirm Password</label>
-                    <input type="password" />
+                    <input type="password" name="confirmPassword" value={confirmPassword} onChange={changeHandler}/>
                 </div>
 
                 <div>
                     <label>Accept our policies</label>
-                    <input type="text" />
+                    <input type="checkbox" name="isAccepted" value={isAccepted} onChange={changeHandler}/>
                 </div>
 
                 <div>
